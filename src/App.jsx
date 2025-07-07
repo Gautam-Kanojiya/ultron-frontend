fd5dee316dfb3458fb51d630029706711f397dd9
MANISHAW19 <themanishaw07@gmail.com>
2025-07-07 15:10:02 +0530

    about page added

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import TransporterDashboard from './pages/TransporterDashboard';
 import Home from './pages/Home'; // You can create this as a landing page
+import AboutPage from './pages/aboutUs';
 
 function App() {
   return (
@@ -8,6 +9,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/transporter-dashboard" element={<TransporterDashboard />} />
+        <Route path="/about-us" element={<AboutPage />} />
       </Routes>
     </Router>
   );
