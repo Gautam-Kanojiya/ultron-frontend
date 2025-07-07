ddd230d22526718e15a90f6b4f9204520ecf3a74
MANISHAW19 <themanishaw07@gmail.com>
2025-07-08 00:35:14 +0530

    navbar fixed

--- src/components/ui/navBar.jsx
+++ src/components/ui/navBar.jsx
@@ -53,25 +53,26 @@ export default function Navbar() {
   <div className={`bg-white border border-white/20 rounded-xl absolute top-full left-0 w-full px-6 py-6 md:hidden shadow-lg transition-all duration-300 ease-in-out ${isOpen ? 'block' : 'hidden'}`}>
     {/* Centered Nav Links */}
     <div className="flex flex-col items-center justify-center space-y-2 text-[#1e1b18] font-medium">
-      <a href="#" className="hover:text-[#8bb5f4] transition">Home</a>
-      <a href="#" className="hover:text-[#8ec5ff] transition">Our Story</a>
-      <a href="#" className="hover:text-[#9193ad] transition">Join Us</a>
+      <a href="/" className="hover:text-[#8bb5f4] transition">Home</a>
+      <a href="/our-story" className="hover:text-[#8ec5ff] transition">Our Story</a>
+      <a href="/join-us" className="hover:text-[#9193ad] transition">Join Us</a>
     </div>
 
     <hr className="my-4" />
 
   {/* Auth Buttons */}
   <div className="flex flex-col items-center space-y-3">
-    <button className="text-[#1e1b18] hover:text-[#3e92cc] font-semibold">Sign In</button>
-    <button className="bg-[#d8315b] hover:bg-[#b92549] text-white px-4 py-2 rounded-full text-sm font-semibold shadow">
+    <Link to={"/sign-in"}><button className="text-[#1e1b18] hover:text-[#3e92cc] font-semibold">Sign In</button></Link>
+    <Link to={"/sign-up"}><button className="bg-[#d8315b] hover:bg-[#b92549] text-white px-4 py-2 rounded-full text-sm font-semibold shadow">
       Get Started
     </button>
+    </Link>
   </div>
 
   <hr className="my-4" />
 
   {/* Search Bar */}
-  <div className="flex items-center justify-center">
+  {/* <div className="flex items-center justify-center">
     <div className="flex items-center border border-gray-300 rounded-full px-3 py-1 bg-white">
       <Search className="h-4 w-4 text-gray-500 mr-2" />
       <input
@@ -80,7 +81,7 @@ export default function Navbar() {
         className="outline-none text-sm bg-transparent w-32"
       />
     </div>
-  </div>
+  </div> */}
 </div>
 
     </nav>
